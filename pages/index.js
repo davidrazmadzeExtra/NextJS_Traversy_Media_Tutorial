f21c3ecada00a4492272a485962d6fdc12c18ead
David Razmadze <razd45@gmail.com>
2022-03-14 20:42:36 -0400

    Add HTML5 Header

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,12 @@ import Head from "next/head";
 export default function Home() {
   return (
     <div>
+      {/* HTML 5 Header Tags  */}
+      <Head>
+        <title>WebDev Newz</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
+
       <h1>Welcome to Next </h1>
     </div>
   );
