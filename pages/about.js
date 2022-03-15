1d1f1ea057ac900b84f2df8437017e49790f4f3f
David Razmadze <razd45@gmail.com>
2022-03-14 20:43:12 -0400

    Add title tag to about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Head from "next/head";
 
 const about = () => {
-  return <div>about</div>;
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      about
+    </div>
+  );
 };
 
 export default about;
